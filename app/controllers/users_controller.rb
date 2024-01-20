33993be71759ffbfc928bf87bb41945e9ea947ff
Lucas Mazala <lucas_mazala@hotmail.com>
2024-01-20 20:23:16 -0300

    Creating a new user - Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new 
+    @user = User.new
+    end
+    
+  def create 
+    @user = User.new(user_params)
+    
+    if @user.save 
+      redirect_to @user
+    else 
+      render :new, status: :unprocessable_entity # the fields will be still filled out
+    end
+  end
+  private 
+    #strong parameters for safety
+    def user_params 
+      params.require(:user).permit(:name, :coordiantion, :telephone, :email, :company, :cooperator)
+    end
+
 end
+
 
