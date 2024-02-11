fde3338e69ff34b2582ab138a4fbae7b00da6960
Lucas Mazala <lucas_mazala@hotmail.com>
2024-02-11 16:49:47 -0300

    Adding type column to users - Becoming a STI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_11_193049) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_11_194647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_193049) do
     t.string "company"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
 end
