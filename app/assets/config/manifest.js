1e46ed032a416209e292ca15b924135fa9983991
Lucas Mazala <lucas_mazala@hotmail.com>
2024-05-12 19:24:52 -0300

    Limiting the table height on the page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+
+// This was added to use a customized css. 
+//= link application.bootstrap.css
